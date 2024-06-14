84af58a99227421338dc06cf32722ae1e3751b8f
whelew <lewisswheelerr@outlook.com>
2024-06-14 14:23:35 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswers() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractionQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function displayDivisionQuestion() {
+    
+}
